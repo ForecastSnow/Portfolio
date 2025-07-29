a9c7dc69642f8edc8074c504a8f268ec475cfab0
ForecastSnow <tomasperezpsn@outlook.com>
2025-07-29 06:02:59 -0300

    svgr FIX

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()]
+  plugins: [svgr(),react(), tailwindcss()]
 })
